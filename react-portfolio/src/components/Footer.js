8b6c779152fd8a95755b3ef950b0a522e55435a6
seth skinner <sethwskinner@gmail.com>
2020-10-01 16:01:00 -0500

    building out footer component

---
+++ react-portfolio/src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+const Footer = () => {
+
+    return(
+
+        <footer className='mt-5'>
+
+            <Container fluid={true}>
+
+                <Row className='border-top justify-content-between p-3'>
+
+                    <Col className='p-0' md={3} sm={12}>                        
+                        Seth Skinner
+                    </Col>
+                    <Col className='p-0 d-flex justify-content-end' md={3}>                        
+                        This site was made by @SethSkinner
+                    </Col>
+
+                </Row>
+
+            </Container>
+
+        </footer>
+
+    );
+
+}
+
+export default Footer;
