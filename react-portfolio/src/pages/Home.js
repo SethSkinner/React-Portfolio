62855282dfe612668bdaa1b17cb5dfc4e150a05f
seth skinner <sethwskinner@gmail.com>
2020-10-01 15:05:41 -0500

    building structure of home page

---
+++ react-portfolio/src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Carousel from '../components/Carousel';
+import Hero from '../components/Hero';
+
+const Home = (props) => {
+
+    return(
+
+        <div>
+
+            <Hero title={props.title} subTitle={props.subTitle} text={props.text}/>
+
+            <Carousel/>
+
+        </div>
+
+    );
+
+}
+
+export default Home; 
