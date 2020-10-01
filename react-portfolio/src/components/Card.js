36b64ef2710a1d5583edfabb863e6783d13039f5
seth skinner <sethwskinner@gmail.com>
2020-10-01 17:22:56 -0500

    fixing bootstrap

--- react-portfolio/src/components/Card.js
+++ react-portfolio/src/components/Card.js
@@ -6,7 +6,7 @@ const Card = (props) => {
 
     return(
 
-        <div className='d-inline-block g-card' onClick={(e) => props.clock(props.item)}>
+        <div className='d-inline-block g-card' onClick={(e) => props.click(props.item)}>
         
             <img className='g-card-image' src={props.item.imgSrc} alt={props.item.imgSrc}/>
             
